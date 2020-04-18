1c03db2696c426a9cbbfa585a21e061a318a534b
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2020-04-18 11:49:20 -0400

    appConfig implemented

---
+++ eCommerceApi/AppConfig.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using WooCommerceNET;
+
+namespace ApiCore
+{
+    public class AppConfig : IDisposable
+    {
+        #region FIELDS
+        private static AppConfig _instance;
+        private static string _connectionString;
+        //private static IDbConnectionFactory _dbFactory;
+        //private Database _db = null;
+
+        private RestAPI _restApi = null;
+
+        //public static string code;
+        //public static string realmId;
+        //public static TokenResponse Token;
+
+        private static string clientid;
+        private static string clientsecret;
+        private static string redirectUrl;
+        private static string environment;
+        private static string baseUrl;
+
+
+        //private static OAuth2Client _auth2Client = null;
+
+        //public  OAuth2Client Auth2Client 
+        //{
+        //    get
+        //    {
+        //        return _auth2Client;
+        //    }
+        //}
+
+
+        #endregion
+
+        #region CTOR
+        private AppConfig()
+        {
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var conf = builder.Build();
+           // _connectionString = conf.GetConnectionString("DefaultConnection");
+            clientid = conf.GetSection("WooCommerceSettings").GetValue(typeof(string), "clientid").ToString();
+            clientsecret = conf.GetSection("WooCommerceSettings").GetValue(typeof(string), "clientsecret").ToString();          
+            baseUrl = conf.GetSection("WooCommerceSettings").GetValue(typeof(string), "baseUrl").ToString();
+          
+
+
+
+            _instance = null;
+            //_dbFactory = new OrmLiteConnectionFactory(_connectionString, SqlServer2014Dialect.Provider);
+        }
+        #endregion
+
+
+        #region GETTERS AND SETTERS
+        public static AppConfig Instance()
+        {
+            if (_instance == null)
+                _instance = new AppConfig();
+
+            return _instance;
+        }
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+            set
+            {
+                _connectionString = value;
+            }
+        }
+
+        //public Database Db
+        //{
+        //    get
+        //    {
+        //        if (_db == null)
+        //            _db = new Database(ConnectionString);
+
+        //        return _db;
+        //    }
+        //}
+
+        public RestAPI Service
+        {
+            get
+            {
+                if (_restApi == null)
+                    _restApi = new  RestAPI(baseUrl, clientid, clientsecret);
+
+                return _restApi;
+            }
+        }
+
+
+
+        //public IDbConnectionFactory DbFactory { get { return _dbFactory; } }
+
+
+
+
+        #endregion
+
+        #region PUBLIC METHODS
+        public void Dispose()
+        {
+            _instance = null;
+            _connectionString = null;
+            //_dbFactory = null;
+        }
+
+        #endregion
+
+
+
+    }
+}
--- eCommerceApi/Controllers/eProductController.cs
+++ eCommerceApi/Controllers/eProductController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiCore;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WooCommerceNET;
@@ -16,9 +17,9 @@ namespace eCommerceApi.Controllers
     {
         public async  Task<IActionResult> Get()
         {
-
-            RestAPI rest = new RestAPI("http://52.71.3.144/wp-json/wc/v3/", "ck_e3763a72e7b9458a6540fdf48d42097db87106cd", "cs_1f204be5002a0d673d7d91e78a6121cfdb908919");
-            WCObject wc = new WCObject(rest);
+            var restApi = AppConfig.Instance().Service;
+     //       RestAPI rest = new RestAPI("http://52.71.3.144/wp-json/wc/v3/", "ck_e3763a72e7b9458a6540fdf48d42097db87106cd", "cs_1f204be5002a0d673d7d91e78a6121cfdb908919");
+            WCObject wc = new WCObject(restApi);
 
             //Get all products
             var products = await wc.Product.GetAll();
