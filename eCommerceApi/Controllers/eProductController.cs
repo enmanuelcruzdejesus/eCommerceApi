2b39c6a8769aab2dca4ef187368b48a0c2fd11c9
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2020-04-18 11:21:45 -0400

    Add project files.

---
+++ eCommerceApi/Controllers/eProductController.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WooCommerceNET;
+using WooCommerceNET.WooCommerce.v3;
+
+namespace eCommerceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class eProductController : ControllerBase
+    {
+        public async  Task<IActionResult> Get()
+        {
+
+            RestAPI rest = new RestAPI("http://52.71.3.144/wp-json/wc/v3/", "ck_e3763a72e7b9458a6540fdf48d42097db87106cd", "cs_1f204be5002a0d673d7d91e78a6121cfdb908919");
+            WCObject wc = new WCObject(rest);
+
+            //Get all products
+            var products = await wc.Product.GetAll();
+
+            return Ok(products);
+        }
+    }
+}
