f1600ed122bc3282e5f1975327a6f7c8621342b6
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2020-04-20 19:52:20 -0400

    models added

---
+++ eCommerceApi/Model/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceApi.Model
+{
+    public class OrderDetail
+    {
+        public int id { get; set; }
+        public int productId { get; set; }
+        public string descrip { get; set; }
+        public int quantity { get; set; }
+        public decimal price { get; set; }
+        public int subtotal { get; set; }
+        public string tax_class { get; set; }
+        public decimal subtotal_tax { get; set; }
+        public decimal total_tax { get; set; }
+        public decimal total { get; set; }
+    }
+}
---
+++ eCommerceApi/Model/ProductCategory.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceApi.Model
+{
+    public class ProductCategory
+    {
+      
+        public int id { get; set; }
+        public string descrip { get; set; }
+        public string slug { get; set; }
+    }
+}
